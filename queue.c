dfe2488888a091822f76f5398a3d716d9c972ef4
Build-A-Moat <ji394su303196@gmail.com>
2022-02-20 21:21:00 +0800

    Fix: fix q_remove_tail

--- queue.c
+++ queue.c
@@ -119,7 +119,7 @@ element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
     if (!head || list_empty(head))
         return NULL;
     element_t *remove = list_last_entry(head, element_t, list);
-    list_del(head->next);
+    list_del(head->prev);
     if (sp) {
         strncpy(sp, remove->value, bufsize - 1);
         sp[bufsize - 1] = '\0';
